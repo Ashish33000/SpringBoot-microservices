b2ec58d305d6efca60ae0b026c4c60032a2c120e
Ashish Kumar <ashishbhagat330@gmail.com>
2023-03-07 16:48:18 +0530

    added

--- ApiGateway/src/main/java/com/masai/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/masai/ApiGateway/ApiGatewayApplication.java
@@ -1,13 +1,11 @@
-package com.masai;
+package com.masai.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@LoadBalancerClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
